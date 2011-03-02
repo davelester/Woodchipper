605ddc28fd9806d6721613c52006a2cedf461b08
Dave Lester <davelester@gmail.com>
2011-03-02 10:18:53 -0500

    First commit of Woodchipper Client and raphaeljs, including sample data.

---
+++ woodchipper.js
@@ -0,0 +1,12 @@
+$(window).load(function () {
+	// using the example from raphaeljs.com
+		
+	// Creates canvas 320 × 200 at 10, 50
+	var paper = Raphael(10, 50, 320, 200);
+
+	// Creates circle at x = 50, y = 40, with radius 10
+	var circle = paper.circle(50, 40, 10);
+	// Sets the fill attribute of the circle to red (#f00)
+	circle.attr("fill", "#f00");
+
+});
